34cb9f1fdb6317118e666510056ce8aed06d9347
anuj-snatak <anuj.jain@mygurukulam.co>
2026-03-30 17:18:33 +0530

    Update main.tf

--- spacelift-poc/main.tf
+++ spacelift-poc/main.tf
@@ -20,7 +20,7 @@ data "aws_ami" "amazon_linux" {
 
 resource "aws_instance" "demo" {
   ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name        = "spacelift-poc"
